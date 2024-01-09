9ecebddc362f8c1b85740a614e8441b01aebeac9
Kyle Teopiz <kylecteopiz@gmail.com>
2024-01-09 14:34:07 -0500

    Attempt on Longest Substring question Left comments on current thought process for later completetion First issue with pointer posiiton and where to place them for each case Second ussue with populating set with the correct letters based on pointers

--- strings.py
+++ strings.py
@@ -14,6 +14,47 @@ def is_palindrome(x):
                 start += 1
                 end -= 1
         return True 
+def lengthOfLongestSubstring(s):
+
+        letter_set = set()
+
+        longest, slow, fast = 0, 0, 0
+
+        # Edge Cases for small strings
+        if len(s) == 0:
+            return 0
+        elif len(s) == 1:
+            return 1
+        
+        while fast < (len(s)):
+            if s[fast] in letter_set:
+                # Save length of longest substring if it is larger than the current max length substring
+                if len(letter_set) > longest:
+                    longest = len(letter_set)
+                # Reset the letter set
+                letter_set = set()
+                # Pull the slow pointer forward to check
+                slow = fast
+            else:
+                letter_set.add(s[fast])
+                fast += 1
+        print(letter_set)
+        if longest < len(letter_set):
+             return len(letter_set)
+        else:
+             return longest
 
 if __name__ == "__main__":
-     pass
+    # Current Case does not work couple ideas on why:
+    # 1) Set updation -> Right now we are totally resetting the set once we find a duplicate ...
+    # Achieve this by jumping the slow poitner to the fast one, this skips over a letter that belongs to the longest substring
+    # Currently -> 407/987 test cases passing on LC
+    s = "dvdf"
+
+    print(lengthOfLongestSubstring(s))
+    
+
+
+
+
+
